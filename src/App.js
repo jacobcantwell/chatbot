f489b60b22702134c2cb348826bdb9dfab5ed652
Jacob Cantwell <jaccantw@amazon.com>
2019-11-22 01:56:32 +1100

    Updating Lex chat bot look

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import LexChat from 'react-lex';
-import './App.css';
+import LexChat from './components/lex/LexChat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>SA SOS</h1>
-      </header>
-      <div>
-        <LexChat botName="AWSSA"
-          IdentityPoolId="us-east-1:3ec15448-603e-4501-a061-f24a15ff989e"
-          placeholder="Placeholder text"
-          style={{position: 'absolute'}}
-          backgroundColor="#FFFFFF"
-          height="600"
-          headerText="Chat with our awesome bot" />
-      </div>
-    </div>
+    <LexChat botName="AWSSA"
+      IdentityPoolId="us-east-1:3ec15448-603e-4501-a061-f24a15ff989e"
+      placeholder="Enter an AWS service name"
+      style={{position: 'absolute'}}
+      backgroundColor="#FFFFFF"
+      height={400}
+      headerText="Chat with the SA SOS bot" />
   );
 }
 export default App;
